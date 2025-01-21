4801ce5288bf4ac3fcb3b5af0c3c20b2df9b8f5e
Daniel Ciuraru <daniel.ciuraru@aquasec.com>
2025-01-21 11:49:49 +0200

    test script

---
+++ test.sh
@@ -0,0 +1,35 @@
+echo "Start test"
+echo $(date +%s)
+
+CONTAINER_ID=$( \
+export AQUA_KEY=ZV0zqahzSr6QYw9DZBdp21
+export AQUA_SECRET=hTuPsIFZioQAuW5Jx7vdCOne2VzBVqxBMPt
+export TRIVY_RUN_AS_PLUGIN=aqua
+export CSPM_URL=https://stage.api.cloudsploit.com
+export AQUA_URL=https://api.dev.supply-chain.cloud.aquasec.com
+export WORKSPACE=$PWD
+export SKIP_CODE_METADATA=true
+docker run -d -p 8082:80 -v /Users/danielciuraru/Library/Caches/trivy:/tmp/.cache/trivy -it \
+-e AQUA_KEY \
+-e AQUA_SECRET \
+-e TRIVY_RUN_AS_PLUGIN \
+-e CSPM_URL \
+-e AQUA_URL \
+-e INPUT_WORKING_DIRECTORY=/scanning \
+-v "$WORKSPACE":"/scanning" \
+aquasec/aqua-scanner:latest \
+trivy fs --scanners vuln,misconfig,secret --sast --output res.json --format json --use-trivy-server --trivy-server-url https://us-east-1.staging.edge.cloud.aquasec.com  .)
+
+# Monitor the container's network I/O every 0.5 second while it's running
+while [ "$(docker inspect -f '{{.State.Running}}' "$CONTAINER_ID")" == "true" ]; do
+    # Extract only the Network I/O information and print it
+    NETWORK_IO=$(docker stats "$CONTAINER_ID" --no-stream --format "{{.NetIO}}")
+    echo "Network I/O: $NETWORK_IO"
+    sleep 0.5
+done
+
+# Wait for the container to exit completely
+docker wait "$CONTAINER_ID"
+
+echo "End test"
+echo $(date +%s)
